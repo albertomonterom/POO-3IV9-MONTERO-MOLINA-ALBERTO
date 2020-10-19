25c90cff49f929542385ba63ba598076e1d1d5df
Alberto Montero Molina <monteromolinaalberto@gmail.com>
2020-10-19 12:28:31 -0500

    Ejemplo de holamundo

---
+++ holamundo/holamundo.java
@@ -0,0 +1,10 @@
+class holamundo{
+
+    public static void main(String[] args){
+
+        String nombre="que onda mis pibes soy Montero";
+
+        System.out.println("holamundo");
+        System.out.println(nombre);
+    }
+}
